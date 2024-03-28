1350693a17e689b4576316b6a26ddc5a7f717cf2
URANI <engineering@urani.xyz>
2024-03-28 12:03:27 +0100

    add fs first lab

--- demos/frontend/01_connecting_to_the_blockchain/node_modules/@coral-xyz/anchor/dist/esm/utils/bytes/base64.js
+++
@@ -1,8 +0,0 @@
-import { Buffer } from "buffer";
-export function encode(data) {
-    return data.toString("base64");
-}
-export function decode(data) {
-    return Buffer.from(data, "base64");
-}
-//# sourceMappingURL=base64.js.map
--- demos/frontend/01_connecting_to_the_blockchain/node_modules/@coral-xyz/anchor/dist/esm/utils/sha256.js
+++
@@ -1,5 +0,0 @@
-import { sha256 } from "@noble/hashes/sha256";
-export function hash(data) {
-    return new TextDecoder().decode(sha256(data));
-}
-//# sourceMappingURL=sha256.js.map
