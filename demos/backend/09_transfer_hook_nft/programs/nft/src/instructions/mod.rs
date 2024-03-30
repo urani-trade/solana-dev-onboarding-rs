51ef0974b39f92b223cc411a85b696aba88c6c6c
URANI <engineering@urani.xyz>
2024-03-30 19:33:17 +0100

    work

--- demos/backend/09_transfer_hook_nft/programs/nft/src/instructions/mod.rs
+++ demos/backend/09_transfer_hook_nft/programs/nft/src/instructions/mod.rs
@@ -1,9 +1,9 @@
-pub use manage_rule::*;
+pub use rules::*;
 pub use mint_membership_nft::*;
 pub use manage_time::*;
 pub use burn_membership::*;
 
-mod manage_rule;
+mod rules;
 mod mint_membership_nft;
 mod manage_time;
 mod burn_membership;
